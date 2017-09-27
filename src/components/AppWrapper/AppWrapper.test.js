3444b663a2d07df07ea7f229a8224f86ebc14223
Daniel Guerra <daniel@densitylabs.io>
2017-09-27 13:39:35 -0500

    add enzyme (not so) unit tests

--- src/components/AppWrapper/AppWrapper.test.js
+++ src/components/AppWrapper/AppWrapper.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './index';
+import { mount } from 'enzyme';
+import AppWrapper from './index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+const getWrapper = () =>
+  mount(<AppWrapper />);
+
+test('renders without crashing', () => {
+  const wrapper = getWrapper();
+  expect(wrapper).not.toBeNull();
+});
+
+test('renders the logo', () => {
+  const wrapper = getWrapper();
+  expect(wrapper.find('.App-logo').length).toBe(1);
+});
+
+test('renders the title', () => {
+  const wrapper = getWrapper();
+  expect(wrapper.find('.App-header').text()).toEqual('Pokemon List');
 });
